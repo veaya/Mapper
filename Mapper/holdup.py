5739309940435d7f7f1f34394dec8119fc46029e
veaya <dhavidgogi@gmail.com>
2021-11-16 18:36:55 -0500

    Code that does not work

--- Mapper/holdup.py
+++ Mapper/holdup.py
@@ -1,9 +1,8 @@
 import numpy as np
 import os
 from PIL import Image
-from process import what 
+from process import processIMG 
 #if we try to open a big file we can shut pillow up
-Image.warnings.simplefilter('error', Image.DecompressionBombWarning)
 
 try:
     max = int(input("Enter the maximum height: "))
@@ -18,25 +17,23 @@ except ValueError:
 difference = int(abs(max - min))
 #blah blah how do I even do this
 #split the difference into 256 separate parts
-interval = int(difference/256)
-#you can already tell that this is being created without accuracy in mind
-#simply due to the limitations of rgb
+
+interval = difference/256
+#needs to be a precise number
+
 try:
     level = int(input("What height do you want the sea level to rise to?: "))
 except ValueError:
     print("Whoops that's not an int")
     level = int(input("What height do you want the sea level to rise to?: "))
+#get file from the user
 user_input = input("Enter the path of your file: ")
 assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
-f = open(user_input,'r+')
 print("OK file exists")
 img = Image.open(user_input)
-
-print(img.format)
-
-what(user_input, level, interval)
-#stuff you do with the file goes here
-f.close()
+width, height = img.size
+print("Your image is a ",width," x ",height," ", img.format, " file")
+processIMG(img, user_input, level, interval)
 
 
 
