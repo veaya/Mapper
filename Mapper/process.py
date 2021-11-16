369200ea2218488bd6032307cc194663a41fb88f
veaya <dhavidgogi@gmail.com>
2021-11-16 18:36:27 -0500

    Code that simply does not work

--- Mapper/process.py
+++ Mapper/process.py
@@ -1,3 +1,16 @@
+#helper class that processes the file
 import numpy as np
-def what(fileLocation, SeaLevelRise, interval):
-    print("yup")
+from PIL import Image
+
+def processIMG(Image, fileLocation, SeaLevelRise, interval):
+    delta = abs(int(interval * SeaLevelRise))
+    Image.show()
+    #hooray we get our maximum RGB value
+    print("Processing Image....")
+    #numpy array because python lists are slow
+    pixels = np.array(Image)
+    red, green, blue= pixels.T 
+    lowAreas = (red<delta) &(blue<delta) &(red<delta)
+    pixels[..., :-1][lowAreas.T] = (255, 0, 0)
+    im2 = Image.fromarray(pixels)
+    im2.show()
