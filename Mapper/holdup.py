1c202c73e6b6b566fd5becf921163139f3df585e
veaya <dhavidgogi@gmail.com>
2021-11-16 10:55:04 -0500

    More code

--- Mapper/holdup.py
+++ Mapper/holdup.py
@@ -1,3 +1,9 @@
+import numpy as np
+import os
+from PIL import Image
+from process import what 
+#if we try to open a big file we can shut pillow up
+Image.warnings.simplefilter('error', Image.DecompressionBombWarning)
 
 try:
     max = int(input("Enter the maximum height: "))
@@ -15,6 +21,26 @@ difference = int(abs(max - min))
 interval = int(difference/256)
 #you can already tell that this is being created without accuracy in mind
 #simply due to the limitations of rgb
+try:
+    level = int(input("What height do you want the sea level to rise to?: "))
+except ValueError:
+    print("Whoops that's not an int")
+    level = int(input("What height do you want the sea level to rise to?: "))
+user_input = input("Enter the path of your file: ")
+assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
+f = open(user_input,'r+')
+print("OK file exists")
+img = Image.open(user_input)
+
+print(img.format)
+
+what(user_input, level, interval)
+#stuff you do with the file goes here
+f.close()
+
+
+
+
 
 
     
---
+++ Mapper/process.py
@@ -0,0 +1,3 @@
+import numpy as np
+def what(fileLocation, SeaLevelRise, interval):
+    print("yup")
